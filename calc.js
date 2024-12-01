ecff46519c14d5eb8988ac94b6851097494b3362
shimizukobo <43zutaka32@gmail.com>
2024-12-01 18:13:55 +0900

    Update calc.js

--- calc.js
+++ calc.js
@@ -108,7 +108,7 @@ function renderPlaces(places, pos) {
         else{
             jsonAltitude = jsonAltitude - 33;
         }
-alert("\nちゃんと撮れるかな ver1.0.0\n須ノ川のクリスマスツリーを見るブラウザAR\n緯度 " + pos.coords.latitude + "\n経度 " + pos.coords.longitude + "\n標高 " + jsonAltitude + "\nボタンをタップすると撮影できます。\n\n初回の起動時には、位置情報を取得がうまくいかない場合は、\n少し時間をおいてブラウザの更新をしてください。");
+alert("\nちゃんと撮れるかな ver1.0.1\n須ノ川のクリスマスツリーを見るブラウザAR\n緯度 " + pos.coords.latitude + "\n経度 " + pos.coords.longitude + "\n標高 " + jsonAltitude + "\nボタンをタップすると撮影できます。\n\n初回の起動時には、位置情報を取得がうまくいかない場合は、\n少し時間をおいてブラウザの更新をしてください。");
     
     places.forEach((place) => {
         let latitude = place.location.lat;
@@ -130,7 +130,7 @@ jsonAltitude = -jsonAltitude;
             model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
         }
         model.setAttribute('gltf-model', `${modelName}`);
-        model.setAttribute('position', '0 '+jsonAltitude+' 0');
+//        model.setAttribute('position', '0 '+jsonAltitude+' 0');
         model.setAttribute('animation-mixer', '');
         if(cal.distance >= 500){
             model.setAttribute('scale', `${cal.objectSize}`);
